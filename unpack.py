1c0e9505a9e46b8535f044541e5cd25a09b389e9
shreeya917 <shreeyaregmi6@gmail.com>
2017-12-18 18:25:22 +0545

    Unpack Completed

--- unpack.py
+++ unpack.py
@@ -1,15 +1,18 @@
 # Q5. Code a Function that simply returns ("Hello", 45, 23.3)and call this function and unpack the returned values and print it.
 
+
+
+
 def f():
-    return ("Hello", 45, 23.3)
 
-
-x, y, z = f()
-print(x)
-print(y)
-print(z)
+    return ["Hello", 45, 23.3]
 
 
 
+result = list(f())
+
+
+
+print(result)
 
 #x,y,z=unpack()
